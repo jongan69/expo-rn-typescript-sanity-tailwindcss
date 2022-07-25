4f373a9c8cfeb9a8027db7d7c29ea2e792ee3d20
Jonathan Gan <jonathan.gan@chain-bytes.com>
2022-07-25 00:52:56 -0400

    morgan freeman

--- src/lib/createUser.js
+++ src/lib/createUser.js
@@ -2,16 +2,16 @@ import { client } from './client'
 
 const createUserOnSanity = async (walletAddress) => {
   try {
-    const defaultProfileImage = `https://avatars.dicebear.com/api/identicon/${walletAddress}.svg`
 
-
+    // Default to Morgan Freeman
+    const defaultProfileImage = `https://imgs.search.brave.com/aYg-RzyawkbqVwWMvZEtIc_RWGaVgwhroURVh9YIkaE/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9iaW9h/Z2V3aG8uY28vd3At/Y29udGVudC91cGxv/YWRzLzIwMTkvMDYv/TW9yZ2FuLUZyZWVt/YW4tV2lraS1OZXQt/V29ydGgtV2lmZS1D/aGlsZC1DaGlsZHJl/bi1LaWRzLUZhY3Rz/LUJyb3RoZXIuanBl/Zw`
 
     const userDoc = {
       _type: 'users',
       _id: walletAddress,
       name: walletAddress,
       walletAddress: walletAddress,
-      defaultProfileImage: defaultProfileImage,
+      profileImage: defaultProfileImage,
     }
 
     const sanityResponse = await client.createIfNotExists(userDoc)
