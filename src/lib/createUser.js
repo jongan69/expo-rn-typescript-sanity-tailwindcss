a33aba5e217f257b8dc315d1cace6ce39b9d0a40
Jonathan Gan <jonathan.gan@chain-bytes.com>
2022-07-25 01:15:38 -0400

    Morgan freeman

--- src/lib/createUser.js
+++ src/lib/createUser.js
@@ -4,14 +4,20 @@ const createUserOnSanity = async (walletAddress) => {
   try {
 
     // Default to Morgan Freeman
-    const defaultProfileImage = `https://imgs.search.brave.com/aYg-RzyawkbqVwWMvZEtIc_RWGaVgwhroURVh9YIkaE/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9iaW9h/Z2V3aG8uY28vd3At/Y29udGVudC91cGxv/YWRzLzIwMTkvMDYv/TW9yZ2FuLUZyZWVt/YW4tV2lraS1OZXQt/V29ydGgtV2lmZS1D/aGlsZC1DaGlsZHJl/bi1LaWRzLUZhY3Rz/LUJyb3RoZXIuanBl/Zw`
+    const defaultProfileImage = `image-7ee657e888f359e353f596c3e9c9f07c3178f3e2-3338x3339-jpg`
 
     const userDoc = {
       _type: 'users',
       _id: walletAddress,
       name: walletAddress,
       walletAddress: walletAddress,
-      profileImage: defaultProfileImage,
+      profileImage: {
+        _type: 'image',
+        asset: {
+          _type: "reference",
+          _ref: defaultProfileImage
+        }
+      }
     }
 
     const sanityResponse = await client.createIfNotExists(userDoc)
